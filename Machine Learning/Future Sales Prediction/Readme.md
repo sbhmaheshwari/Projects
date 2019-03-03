1272303133410c788e67d99bef733b7e2419281d
Saurabhmaheshwari52 <saurabhmaheshwari52@gmail.com>
2019-03-02 21:53:07 -0800

    updates

---
+++ Machine Learning/Future Sales Prediction/Readme.md
@@ -0,0 +1,29 @@
+
+This is a project inspired from the Kaggle competition __'Future Sales Prediction'__, and the data can be found on the link:
+https://www.kaggle.com/c/competitive-data-science-final-project/data
+
+Worked on Windows 10, i-5-3230M CPU @ 2.60GHz, 8 GB RAM, 2 cores. Used __Google Colabs__ for neural network training and __Bayesian optimiztion using hyperopt__ 
+
+In the project, I have used the following models and stacked them:
+1) _Neural network with categorical embeddings_ - Embedding_nn_pfs4.ipynb
+2) _CatBoost_ - pfs4_catboost_optimize.ipynb
+3) _XGBoost_ - XGBOOST_pfs4_tuning.ipynb
+4) _Random forest_ - pfs_rf_tuning4.ipynb
+
+The file for data preparation and feature processing is PFS_Modeling4.ipynb, where the following steps are taken:
+1) Aggregate the data w.r.t [shop, item, date block num], [shop, date block num], [item, date block num] and [item category id, date block num]
+2) Create lag features for 1-6, 12 months for monthly aggregated quantity, sales, price for different grouped features
+3) Create mean encodings for categorical features - item, shop, month, year and item category id
+4) Create text features (5 each) using _truncated svd_ for item names and item category names
+
+Validation Strategy:
+1) Use data for 9, 21 and 33 months for validation
+2) While validating count error over only the shop and item ids that are present in the test data such that they have a similar distribution
+
+Stacking: 
+1) Used linear regression and random forest for stacking, but linear regression gave better results 
+
+What could have been done better:
+1) As in test set all shop id and item id pair occurs, it would have been a good idea to create all the combinations of shop and item ids in a date block num and use a bigger train set. But that would lead to 112M rows, and resources were limited at my end
+2) On going through past projects, came across various examples where LightGBM produced good results, could work here
+3) Using Deeper network and embedding Item ids as well 
---
+++ Machine Learning/Future Sales Prediction/Utils_pfs.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+
+def downcast_dtypes(data):
+    float_cols = [c for c in data if data[c].dtype == "float64"]
+    int_cols =   [c for c in data if data[c].dtype == "int64"]
+    data[float_cols] = data[float_cols].astype(np.float32)
+    data[int_cols]   = data[int_cols].astype(np.int32)
+    return(data)
+
+def create_test(test, sales_test):
+    test = test.loc[(test['shop_id'].isin(sales_test['shop_id']))&(test['item_id'].isin(sales_test['item_id'])),:].copy()
+    test_pred = test['cnt_shop_item'].copy()
+    test.drop('cnt_shop_item', axis = 1, inplace = True)
+    return(test, test_pred)
+    
+def create_submission_file(pred, name, sales_test):
+    ID = np.arange(0, sales_test.shape[0]) 
+    new_df = pd.DataFrame({'ID': ID, 'item_cnt_month': pred})
+    new_df.to_csv('Submission Time Series/'+name+'.csv',index = False)
+    
+def create_cv_sets(months, data, full_data):
+    train = []
+    test = []
+    train_pred = []
+    test_pred = []
+    for i in months:
+        x = data.loc[full_data['date_block_num']<i,:].drop('cnt_shop_item', axis = 1)
+        y_pred = data.loc[full_data['date_block_num']<i,'cnt_shop_item']
+        x_test = data.loc[full_data['date_block_num']==i,:]
+        x_test, y_test_pred = create_test(x_test)
+        train.append(x)
+        test.append(x_test)
+        train_pred.append(y_pred)
+        test_pred.append(y_test_pred)
+    return(train,test,train_pred,test_pred) 
+    
+def create_cv_sets2(months, data, sales_test):
+    X_train = data.loc[~data['date_block_num'].isin(months+[34]),:].drop('cnt_shop_item', axis=1)
+    X_val = data.loc[data['date_block_num'].isin(months),:]
+    X_val, y_val = create_test(X_val, sales_test)
+    X_test = data.loc[data['date_block_num'] == 34,:].drop('cnt_shop_item',axis=1)
+    y_train = data.loc[~data['date_block_num'].isin(months+[34]),'cnt_shop_item']
+    return(dict({'train': X_train, 'val': X_val, 'test': X_test, 'train_y': y_train, 'val_y': y_val}))
+
+def create_test_data(prediction, dict_cat):
+  trtv = pd.concat([dict_cat['train'], dict_cat['val']], axis = 0)
+  test_ids = dict_cat['test']['item_id'].isin(trtv['item_id']) & dict_cat['test']['shop_id'].isin(trtv['shop_id'])
+  prediction[~test_ids] = 0
+  return(prediction)
+
+def error(pred, act):
+    return(np.sqrt(np.mean(np.square(np.subtract(act,pred)))))
